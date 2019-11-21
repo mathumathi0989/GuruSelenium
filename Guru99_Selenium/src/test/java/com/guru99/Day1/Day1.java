2766e359135c9254aff11109f8b68147b9fa23ad
mathu <mathu@DESKTOP-PSPSVDJ.fios-router.home>
2019-11-20 19:18:29 -0500

    Day 2

--- Guru99_Selenium/src/test/java/com/guru99/Day1/Day1.java
+++ Guru99_Selenium/src/test/java/com/guru99/Day1/Day1.java
@@ -1,13 +1,15 @@
 package com.guru99.Day1;
 import java.util.concurrent.TimeUnit;
 
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -15,7 +17,7 @@ public class Day1 {
     static WebDriver driver;
   static String baseURL;
   
-    @BeforeClass
+    @BeforeMethod
     public void tearup() {
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
@@ -25,50 +27,47 @@ public class Day1 {
         driver.manage().timeouts().implicitlyWait(Util.waittime, TimeUnit.SECONDS);
     }
 
-    @Test
-    public void login() {
-    	String username;
-    	String password;
+    @DataProvider
+    public Object[][] getdata() throws InvalidFormatException {
+    Object data[][] = 	Util.getTestData("Data");
+    return data;
+    }
+    @Test(dataProvider = "getdata")
+    public void login(String username, String password) {
     	String actualBoxtitle;
-    	String [][] testData = Util.getdatafromExcel(Util.FILE_PATH, Util.SHEET_NAME, Util.SHEET_NAME);
-    	for (int i = 0; i < testData.length; i++) {
-    	    username = testData[i][0];
-    	    password = testData[i][1]; 
+       try
+        {
     	    tearup();
     	    
-    	    driver.findElement(By.name("uid")).clear();
-        driver.findElement(By.name("uid")).sendKeys(username);
-        driver.findElement(By.name("password")).clear();
-        driver.findElement(By.name("password")).sendKeys(password);
-        
-        driver.findElement(By.name("btnLogin")).submit();
-        
-        
-        try
-        {
-        	Alert alt = driver.switchTo().alert();
-			actualBoxtitle = alt.getText(); // get content of the Alter Message
-			alt.accept();
-			if (actualBoxtitle.contains(Util.EXPECT_ERROR)) { // Compare Error Text with Expected Error Value
-			    System.out.println("Test case SS[" + i + "]: Passed"); 
-			} else {
-			    System.out.println("Test case SS[" + i + "]: Failed");
-			}
+       	 driver.findElement(By.name("uid")).clear();
+           driver.findElement(By.name("uid")).sendKeys(username);
+           driver.findElement(By.name("password")).clear();
+           driver.findElement(By.name("password")).sendKeys(password);
+           
+           driver.findElement(By.name("btnLogin")).submit();
+           
+    	   Alert alt = driver.switchTo().alert();
+        	actualBoxtitle = alt.getText(); // get content of the Alter Message
+        	alt.accept();
+        	if (actualBoxtitle.contains(Util.EXPECT_ERROR)) { // Compare Error Text with Expected Error Value
+        		System.out.println("Test case : Passed"); 
+        	} else {
+        		System.out.println("Test case SS: Failed");
+        	}
         }
         catch (NoAlertPresentException e) {
-        	
-        String atitle = driver.getTitle();
-        if (atitle.contains(Util.etitle)) {
-        	   System.out.println("Test case SS[" + i + "]: Passed");
-        				} else {
-        				    System.out.println("Test case SS[" + i + "]: Failed");
-        				}
-        }
-    	}
+
+        	String atitle = driver.getTitle();
+        	if (atitle.contains(Util.etitle)) {
+        		System.out.println("Test case SS: Passed");
+        	} else {
+        		System.out.println("Test case SS: Failed");
+        	}
+      
+    }
     	
     }
-    
-    @AfterClass
+    @AfterMethod
     public void teardown() {
         driver.quit();
     }
--- Guru99_Selenium/src/test/java/com/guru99/Day1/Util.java
+++ Guru99_Selenium/src/test/java/com/guru99/Day1/Util.java
@@ -1,52 +1,51 @@
 package com.guru99.Day1;
  
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 
-import org.apache.poi.hssf.usermodel.HSSFRow; 
-import org.apache.poi.hssf.usermodel.HSSFSheet;
-import org.apache.poi.hssf.usermodel.HSSFWorkbook; 
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook; 
 
 
 
 public class Util {
-
-	private static final String HSSFCell = null;
 	public  static String  url = "http://demo.guru99.com";
-	//public  static String   username = "mngr233053";
-	//public  static String  password = "qapEqEh";
-	 public static String etitle = "Guru99 Bank Manager HomePage";
+	public static String etitle = "Guru99 Bank Manager HomePage";
     public static int waittime = 30;
-    public static String FILE_PATH = "testData.xls";
-    public static String FILE_NAME = "testData";
-    public static String SHEET_NAME = "Data";
-  public static String EXPECT_ERROR = "User or Password is not valid";
-    public static void main(String... args) throws IOException {
-    
-    	readFromExcel("C:\\Users\\mathu\\eclipse-workspace\\guru99\\Guru99_Selenium\\src\\test\\java\\testData.xls");
-    }
-public static String[][] getdatafromExcel (String FilePath, String SheetName, String FileName){
-	String [][] tabArray = null;
-	//Workbook wb = Workbook.getWorkbook(new File(FilePath));
-	
-	// Workbook wb =  Workbook.;
-	return null ;
-	
-}
-public static void readFromExcel(String file) throws IOException{
-    HSSFWorkbook myExcelBook = new HSSFWorkbook(new FileInputStream(file));
-    HSSFSheet myExcelSheet = myExcelBook.getSheet("Data");
-    
-    HSSFRow row = myExcelSheet.getRow(1);
-    
-    String userName = row.getCell(1).getStringCellValue();
-    String password = row.getCell(2).getStringCellValue();
-    
-    System.out.println(userName + " - " + password);
+    public static String PATH = System.getProperty("user.dir");
+    public static String FILE_PATH = "C:\\Users\\mathu\\git\\GuruSelenium\\Guru99_Selenium\\src\\test\\java\\testData.xls";
+    public static String EXPECT_ERROR = "User or Password is not valid";
+    public static Workbook book;
+    public static Sheet sheet;
   
-    
-    myExcelBook.close();
-    
-}
+public static Object[][] getTestData(String sheetName) throws InvalidFormatException {
+	FileInputStream file = null;
+	try {
+		file = new FileInputStream(FILE_PATH);
+	}
+	catch (FileNotFoundException e) {
+		e.printStackTrace();
+	}
+	try {
+		book =  WorkbookFactory.create(file);
+	}
+	catch (IOException e) {
+		e.printStackTrace();
+	}
+	sheet = book.getSheet(sheetName);
+	Object[][] data = new Object[sheet.getLastRowNum()][sheet.getRow(0).getLastCellNum()];
+	for (int i = 0; i<sheet.getLastRowNum(); i++) {
+		for (int k = 0; k <sheet.getRow(0).getLastCellNum(); k++) {
+			data[i][k] = sheet.getRow(i+1).getCell(k).toString();
+		}
+	}
+	return data;
 
+
+}
 }
